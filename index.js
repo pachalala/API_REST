5ed2c08b7f18d618a3c962c0d0bb439bf058abc4
alvaro.yanes <alvaro.yanes@fosis.gob.cl>
2023-02-24 17:33:05 -0300

    agregando nuevas ruta

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/',(req ,res)=>{
    res.send('hola soy un servidor express :)');
 })
 
+
+app.get('/nueva-ruta',(req ,res)=>{
+    res.send('hola soy nueva ruta ');
+ })
+ 
+
 app.listen(port, ()=>{
 
   console.log('mi port' + port);
