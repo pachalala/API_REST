25a942ad11afdc1397ab0e4ba32e9b79841d825b
alvaro.yanes <alvaro.yanes@fosis.gob.cl>
2023-02-24 17:19:59 -0300

    instalando express y test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('My App');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req ,res)=>{
+   res.send('hola soy un servidor express :)');
+})
+
+app.listen(port, ()=>{
+
+  console.log('mi port' + port);
+ 
+})
