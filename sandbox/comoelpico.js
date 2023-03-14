723dd2af744c3c5fb2dd6897ac8152cb4aae9333
alvaro.yanes <alvaro.yanes@fosis.gob.cl>
2023-03-14 17:39:45 -0300

    un poco mas cerca

--- sandbox/comoelpico.js
+++ sandbox/comoelpico.js
@@ -1,32 +1,31 @@
-
 // https://www.npmjs.com/package/mysql2
-import   mysql   from  'mysql2';
- 
-import {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-  } from './../config.js'
+import mysql from 'mysql2';
 
- 
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT,
+} from './../config.js';
+
 // create the connection to database
 const connection = mysql.createConnection({
-    host:DB_HOST, user: DB_USER, database: DB_NAME,password:DB_PASSWORD,  port: DB_PORT 
+  host: DB_HOST,
+  user: DB_USER,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
 
 // simple query
-  let rout;
-  
-  
-  connection.query(
+let rout;
+
+const [err, results, fields] = connection.query(
   'SELECT * FROM platos',
-  function(err, results, fields) {
-   
-    const row_out =    results.map(       r =>  {
-   
-    /*
+  function (err, results, fields) {
+    const row_out = results.map((r) => {
+      /*
          const [_ingredientes, fields] =      connection.query('SELECT idIngrediente, gramos as grs FROM ingredientes_platos  where idPlato='+r.ID,
             
          function(err, results, fields) {
@@ -39,19 +38,13 @@ const connection = mysql.createConnection({
      //
          return {...r, ingredientes:{_ingredientes}};
       */
-         return {...r, ingredientes:5};
-       
-      
-       });
-        
-console.log(row_out);
+      return { ...r, ingredientes: 5 };
+    });
 
-     
-      rout =   row_out
-    }
-      );
+    console.log(row_out);
 
-   
- 
+    rout = row_out;
+  }
+);
 
- console.log (  rout);
+console.log(results);
--- sandbox/platitos.js
+++ sandbox/platitos.js
@@ -1,42 +1,45 @@
- 
- //import   {pool }  from '../db.js'
- import { createPool}  from  'mysql2/promise' ;
- import {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-  } from './../config.js'
+//import   {pool }  from '../db.js'
+import { createPool } from 'mysql2/promise';
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT,
+} from './../config.js';
 
- 
+const conn = await createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
 
- const conn =  await createPool( {host:DB_HOST, user: DB_USER, database: DB_NAME,password:DB_PASSWORD,  port: DB_PORT});
-  
-  const [rows, fields] = await conn.execute('SELECT * FROM platos' );
+const [rows, fields] = await conn.execute('SELECT * FROM platos');
 
+const [ring, fing] = await conn.execute(
+  'SELECT idPlato, idIngrediente, gramos as grs FROM ingredientes_platos '
+);
 
+const row_out = rows.filter((r) => {
 
-  const row_out =  await rows.map(      r =>  {
-   
-    
-   //  const [_ingredientes, fields] =         conn.execute('SELECT idIngrediente, gramos as grs FROM ingredientes_platos  where idPlato='+r.ID) ;
-//
-   // return {...r, ingredientes:{_ingredientes}};
-   return {...r, ingredientes:5};
-  
- 
+  const _ingredientes = ring.map((ingre) => {
+    if (ingre.idPlato == r.ID) {
+      return [ingre.idIngrediente, ingre.grs];
+    }
   });
 
  
+  return { ...r, ingredientes: _ingredientes };
+  //   return {...r, ingredientes:5};
+});
 
-  console.log (   row_out);
 
+console.log(JSON.stringify(row_out));
 
- //const [rows] =    await  pool.query('SELECT * FROM platos' );
-      
-     
-       
+//const [rows] =    await  pool.query('SELECT * FROM platos' );
+
 /*
 
 
@@ -48,4 +51,4 @@
  }
    )
  
-*/ 
+*/
